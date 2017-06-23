401dffdcad5867193f5b6db04d2263d803d6b0a8
iampeternguyen <ptnguyen1987@gmail.com>
2017-06-23 10:40:35 +0700

    incremental

--- controllers/setup.js
+++ controllers/setup.js
@@ -20,8 +20,6 @@ module.exports = function(slapp){
   });
 
   slapp.route('handleConfirmSetup', (msg, state) => {
-    var text = (msg.body.event && msg.body.event.text) || '' ;
-    state.lang2 = text;
     msg.say('Okay. So you speak ' + state.lang1 + ' and you are learning ' + state.lang2 + '\.');
   });
 
