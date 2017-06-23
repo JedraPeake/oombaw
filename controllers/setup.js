b261af3bd0efad22f7825f2680246bd7be9153ff
iampeternguyen <ptnguyen1987@gmail.com>
2017-06-23 10:05:16 +0700

    incremental

--- controllers/setup.js
+++ controllers/setup.js
@@ -4,9 +4,15 @@ module.exports = function(slapp){
   //setup languages
   slapp.message('setup', 'mention', (msg) => {
     msg.say('What languages do you speak fluently?')
-    .route('handleSecondLang');
-});
-  slapp.route('handleSecondLang', (msg) => {
-    msg.say('What languages are you learning?');
+    .route('handleSecondLang', {a: msg});
   });
+
+  slapp.route('handleSecondLang', (msg) => {
+    msg.say('What languages are you learning? ' + msg.a);
+  });
+
+
+
+
+
 };
